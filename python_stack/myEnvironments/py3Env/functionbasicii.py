72b5f0721538b4b4f539749ec081ad8724e3205a
frednava67 <frednava67@hotmail.com>
2018-10-01 11:53:13 -0700

    function basic ii lab done

--- python_stack/myEnvironments/py3Env/functionbasicii.py
+++ python_stack/myEnvironments/py3Env/functionbasicii.py
@@ -8,7 +8,7 @@ def countDown(i):
 print(countDown(5))
 
 def printFirstReturnSecond(arr):
-    print arr[0]
+    print(arr[0])
     return arr[1]
 
 print(printFirstReturnSecond([25,7]))
@@ -16,4 +16,24 @@ print(printFirstReturnSecond([25,7]))
 def firstPlusLength(arr):
     return arr[0]+len(arr)
 
-print(firstPlusLength([11,22,33,44,55,66,77])
+print(firstPlusLength([11,22,33,44,55,66,77]))
+
+def valuesGreaterThanTheSecond(arr):
+    tempVal = arr[1]
+    retarr = []
+    for i in range(len(arr)):
+        if arr[i] > tempVal:
+            retarr.append(arr[i])
+    else:
+        return retarr
+
+print(valuesGreaterThanTheSecond([1,5,3,4,2,9,8,7,6]))
+
+def lengthAndValue(ilength, ivalue):
+    retarr = []
+    for i in range(ilength):
+        retarr.append(ivalue)
+    else:
+        return retarr
+
+print(lengthAndValue(14,77)) 
