4a1f793b248f9ceb243b5572c1f155cfd8ffc9c6
Fred Nava <frednava67@hotmail.com>
2018-10-01 08:10:39 -0700

    for loop basic 1 lab

--- python_stack/myEnvironments/py3Env/forloopbasics.py
+++ python_stack/myEnvironments/py3Env/forloopbasics.py
@@ -5,19 +5,28 @@
 #    print(x)
 
 
-for i in range(1,101):
-    str = i
-    if i%5==0 :
-        str = ""
-        str = str + "Coding"
-    if i%10==0 :
-        str = str + "Dojo"
-    print(str)
+# for i in range(1,101):
+#     str = i
+#     if i%5==0 :
+#         str = ""
+#         str = str + "Coding"
+#     if i%10==0 :
+#         str = str + "Dojo"
+#     print(str)
 
-for i in range(0,500000):
-    if i%2 == 1:
-        sum = sum + i
+# isum = 0
+# for i in range(0,500000):
+#     if i%2 == 1:
+#         isum = isum + i
 
-print(sum)
-        
+# print(isum)
+
+# for i in range(2018,1,-4):
+#     print(i)
+
+x = 100
+y = 10
+z = -4
+for i in range(x,y,z):
+    print(i)    
         
