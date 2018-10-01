f0b3f18b748ac2208901738ab8e82d143db0ebc6
Fred Nava <frednava67@hotmail.com>
2018-09-30 20:59:23 -0700

    push some python work

---
+++ python_stack/myEnvironments/py3Env/datatypes.py
@@ -0,0 +1,15 @@
+for count in range(0,5):
+    print("looping - ", count)
+
+count = 0
+while count < 5: # notice the colon!
+    print("looping - ", count)
+    count += 1
+
+x = 3
+y = x
+while y > 0:
+    print(y)
+    y = y - 1
+else:
+    print("Final else statement")
---
+++ python_stack/myEnvironments/py3Env/forloopbasics.py
@@ -0,0 +1,23 @@
+#for x in range(151):
+#    print(x)
+
+#for x in range(0,1000001,5):
+#    print(x)
+
+
+for i in range(1,101):
+    str = i
+    if i%5==0 :
+        str = ""
+        str = str + "Coding"
+    if i%10==0 :
+        str = str + "Dojo"
+    print(str)
+
+for i in range(0,500000):
+    if i%2 == 1:
+        sum = sum + i
+
+print(sum)
+        
+        
