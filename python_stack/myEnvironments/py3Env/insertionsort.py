db97a911798b1d5939ff48320bc0cb589295b1c1
frednava67 <frednava67@hotmail.com>
2018-10-01 18:51:42 -0700

    tiny bit of work on the looping structure

---
+++ python_stack/myEnvironments/py3Env/insertionsort.py
@@ -0,0 +1,16 @@
+def insertion_sort(arr):
+    arrLen = len(arr)
+    for curPointerLoc in range(1,arrLen):
+        for innerLoop in range(curPointerLoc-1,-1,-1):
+            print(curPointerLoc, innerLoop)
+    else:
+        return "done"
+
+print("original array: ", [5,6,3,9,2,8])
+print(insertion_sort([5,6,3,9,2,8]))
+print("original array: ", [1,2,3,4,5,6])
+print(insertion_sort([1,2,3,4,5,6]))
+print("original array: ", [19,17,14,12,11,6])
+print(insertion_sort([19,17,14,12,11,6]))
+print("original array: ", [19,17])
+print(insertion_sort([19,17]))
