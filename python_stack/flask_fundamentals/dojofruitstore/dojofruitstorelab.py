52011d6b4ff559004869f95b17317f323648a314
Fred Nava <frednava67@hotmail.com>
2018-10-04 16:46:30 -0700

    lab done

--- python_stack/flask_fundamentals/dojofruitstore/dojofruitstorelab.py
+++ python_stack/flask_fundamentals/dojofruitstore/dojofruitstorelab.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import datetime as dt
 app = Flask(__name__)       
 
 @app.route('/')
@@ -11,8 +12,11 @@ def showResult():
     raspberries_ordered = request.form['raspberry_quantity']
     apples_ordered = request.form['apple_quantity'] 
     order_name = request.form['name']
-    order_student_id = request.form['student_id']    
-    return render_template("ordersummary.html", strawberries_ordered=strawberries_ordered, raspberries_ordered=raspberries_ordered, apples_ordered=apples_ordered, order_name=order_name, order_student_id=order_student_id)    
+    order_student_id = request.form['student_id']
+    total_ordered = int(strawberries_ordered) + int(raspberries_ordered) + int(apples_ordered)   
+    o_date = dt.datetime.now()
+    order_date = o_date.strftime("%c")
+    return render_template("ordersummary.html", order_date=order_date, total_ordered=int(total_ordered), strawberries_ordered=strawberries_ordered, raspberries_ordered=raspberries_ordered, apples_ordered=apples_ordered, order_name=order_name, order_student_id=order_student_id)    
 
 if __name__=="__main__":
     app.run(debug=True)                   # Run the app in debug mode.
