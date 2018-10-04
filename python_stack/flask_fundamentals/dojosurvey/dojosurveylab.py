7d14ed7c9831ae8995da1b41ac9e0504dc1516e9
Fred Nava <frednava67@hotmail.com>
2018-10-04 13:33:11 -0700

    lab done

--- python_stack/flask_fundamentals/dojosurvey/dojosurveylab.py
+++ python_stack/flask_fundamentals/dojosurvey/dojosurveylab.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)       
-
-users = (
-    {'first_name' : 'Michael', 'last_name' : 'Choi'},
-    {'first_name' : 'John', 'last_name' : 'Supsupin'},
-    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-    {'first_name' : 'KB', 'last_name' : 'Tonel'}
-)
 
 @app.route('/')
 def showForm():
-    return render_template('index.html')                            
+    return render_template('index.html')      
+
+@app.route('/result', methods=["POST"])
+def showResult():
+    full_name = request.form['fullname']
+    loc = request.form['location']
+    fav_lang = request.form['favorite_language'] 
+    remarks = request.form['comment']
+    return render_template("display_info.html", full_name=full_name, loc=loc, fav_lang=fav_lang, remarks=remarks)    
 
 if __name__=="__main__":
     app.run(debug=True)                   # Run the app in debug mode.
