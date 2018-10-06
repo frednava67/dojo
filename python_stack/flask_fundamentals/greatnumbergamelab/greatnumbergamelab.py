63fea7b2bbc1dad8a92ca5dddab6c76efc8f627c
Fred Nava <frednava67@hotmail.com>
2018-10-05 21:17:57 -0700

    lab done

--- python_stack/flask_fundamentals/greatnumbergamelab/greatnumbergamelab.py
+++ python_stack/flask_fundamentals/greatnumbergamelab/greatnumbergamelab.py
@@ -7,27 +7,23 @@ app.secret_key = 'antidisestablishmentarianism'
 @app.route('/')
 def index():
     sn = 0
-    guessesclasses = ""
-    print("root route")
     if 'secretnumber' not in session:
-        print("*"*25)
-        print("secretnumber not in session")
         sn = rnd.randrange(0,101)
         print("secret number is ", sn)
         session['secretnumber'] = sn
         session['guessesclasses'] = " hidden "
+        session['btnclasses'] = " hidden "
         session['guess_result'] = ""
-        print(session['guess_result'])
-    print("*"*25)
-    print("secretnumber in session")
-    print(session['secretnumber'])
-    print(session['guessesclasses'])
-    print(session['guess_result'])
-    return render_template('index.html', ctr=session['secretnumber'], divclasses=session['guessesclasses'], guessaccuracy=session['guess_result'])
+        session['userguessdiv'] = " "
+    return render_template('index.html', ugdiv=session['userguessdiv'], playagainbtn=session['btnclasses'], ctr=session['secretnumber'], divclasses=session['guessesclasses'], guessaccuracy=session['guess_result'])
 
 @app.route('/destroy_session')
 def destroyTheSession():
-    print("Destroying Session")
+    session.clear()
+    return redirect('/')
+
+@app.route('/reset_game', methods=['POST'])
+def resetTheGame():
     session.clear()
     return redirect('/')
 
@@ -36,21 +32,18 @@ def check_guess():
     sn = int(session['secretnumber'])
     session['guessesclasses']
     userguess = int(request.form['theguess'])
-    print("the guess was " + str(userguess) + ", the secret number is " + str(sn))
 
     if sn > userguess:
         session['guess_result'] = "Too low!"
-        print(session['guess_result'])
         session['guessesclasses'] = " redbox resultdiv center-block text-center "
-
     elif sn < userguess:
         session['guess_result'] = "Too high!"
-        print(session['guess_result'])
         session['guessesclasses'] = " redbox resultdiv center-block text-center "   
     elif sn == userguess:
         session['guess_result'] = str(sn) + " was the number!"      
         session['guessesclasses'] = " greenbox resultdiv center-block text-center "
-
+        session['btnclasses'] = " btn btn-primary "
+        session['userguessdiv'] = " hidden "
     return redirect('/') 
 
 if __name__ == "__main__":    # If __name__ is "__main__" we know we are running this file directly and not importing
