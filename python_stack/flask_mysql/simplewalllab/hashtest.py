b1176e20b7887d836b7b914c940489942363ee55
Fred Nava <frednava67@hotmail.com>
2018-10-10 12:44:41 -0700

    save out hash testing code

--- python_stack/flask_mysql/simplewalllab/hashtest.py
+++ python_stack/flask_mysql/simplewalllab/hashtest.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt        
 app = Flask(__name__)        
-bcrypt = Bcrypt(app)     # we are creating an object called bcrypt, 
-                         # which is made by invoking the function Bcrypt with our app as an argument
+bcrypt = Bcrypt(app)    # we are creating an object called bcrypt, 
+                        # which is made by invoking the function Bcrypt with our app as an argument
 
 mylist = [{'id': 1, 'email': 'pig@farm.com', 'first_name': 'Wilbur', 'last_name': 'Hogg'}, {'id': 2, 'email': 'spider@farm.com', 'first_name': 'Charlotte', 'last_name': 'Webb'}, {'id': 3, 'email': 'rooster@farm.com', 'first_name': 'Foghorn', 'last_name': 'Leghorn'}]
 print(mylist)
