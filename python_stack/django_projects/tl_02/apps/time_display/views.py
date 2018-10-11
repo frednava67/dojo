c6a0c19aef69398916dda7e2a46469a2f3d2cc66
Fred Nava <frednava67@hotmail.com>
2018-10-10 19:11:36 -0700

    lab done

---
+++ python_stack/django_projects/tl_02/apps/time_display/views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render, HttpResponse, redirect
+import datetime as dt
+
+# the index function is called when root is visited
+def index(request):
+    cur_dt = dt.datetime.now()
+
+    strDate = cur_dt.strftime("%b %d, %Y")
+    strTime = cur_dt.strftime("%I:%M %p")
+
+
+    context = {
+        'date': strDate,
+        'time': strTime
+    }
+
+
+    return render(request, "time_display/index.html", context)
+
