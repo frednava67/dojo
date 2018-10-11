7b73ea0afcfe825db39dbf162f35b2b8f96abd58
Fred Nava <frednava67@hotmail.com>
2018-10-10 17:21:10 -0700

    lab done

--- python_stack/django_projects/toplevel/apps/app_one/urls.py
+++ python_stack/django_projects/toplevel/apps/app_one/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
-    url(r'^$', views.index)   # This line has changed! Notice that urlpatterns is a list, the comma is in
-]                            # anticipation of all the routes that will be coming soon
+    url(r'^$', views.index),
+    url(r'^new$', views.new),
+    url(r'^create$', views.index),
+    url(r'^(?P<blog_number>[0-9]+)$', views.show_blog_number),
+    url(r'^(?P<blog_number>[0-9]+)/edit$', views.edit_blog_number),
+    url(r'^(?P<blog_number>[0-9]+)/delete$', views.destroy),        
+]                          
--- python_stack/django_projects/toplevel/apps/app_one/views.py
+++ python_stack/django_projects/toplevel/apps/app_one/views.py
@@ -8,3 +8,17 @@ def index(request):
     response = "Hello, I am your first request!"
     return HttpResponse(response)
 
+def new(request):
+    response = "placeholder to display a new form to create a new blog"
+    return HttpResponse(response)
+
+def show_blog_number(request, blog_number):
+    response = "placeholder to display blog " + str(blog_number)
+    return HttpResponse(response)    
+
+def edit_blog_number(request, blog_number):
+    response = "placeholder to display blog " + str(blog_number)
+    return HttpResponse(response)
+
+def destroy(request, blog_number):
+    return redirect('/')
