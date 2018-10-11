84c38f02eeb0a3a53a8d0865b8ee747fe3474e70
Fred Nava <frednava67@hotmail.com>
2018-10-10 19:17:19 -0700

    added the time_display route that i forgot about

--- python_stack/django_projects/tl_02/apps/time_display/urls.py
+++ python_stack/django_projects/tl_02/apps/time_display/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
-    url(r'^$', views.index)   # This line has changed! Notice that urlpatterns is a list, the comma is in
-]                            # anticipation of all the routes that will be coming soon
+    url(r'^$', views.index),
+    url(r'^time_display$', views.index),
+]                          
--- python_stack/django_projects/tl_02/apps/time_display/views.py
+++ python_stack/django_projects/tl_02/apps/time_display/views.py
@@ -6,16 +6,11 @@ import datetime as dt
 # the index function is called when root is visited
 def index(request):
     cur_dt = dt.datetime.now()
-
     strDate = cur_dt.strftime("%b %d, %Y")
     strTime = cur_dt.strftime("%I:%M %p")
-
-
     context = {
         'date': strDate,
         'time': strTime
     }
-
-
     return render(request, "time_display/index.html", context)
 
