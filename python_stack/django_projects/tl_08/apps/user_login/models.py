46963e13711fbffb820608adfad0168c909604df
Fred Nava <frednava67@hotmail.com>
2018-10-12 14:53:13 -0700

    push everything before i start working on dojo ninjas

---
+++ python_stack/django_projects/tl_08/apps/user_login/models.py
@@ -0,0 +1,13 @@
+# Inside models.py
+from __future__ import unicode_literals
+
+from django.db import models
+from django.core.validators import validate_email, MinLengthValidator
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)    
+    email_address = models.EmailField()
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
