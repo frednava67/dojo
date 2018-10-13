7a20f97449976069c72efd7eb9e8cc664f448c8a
Fred Nava <frednava67@hotmail.com>
2018-10-12 18:21:55 -0700

    pushing work after books authors

--- python_stack/django_projects/tl_08/tl_08/urls.py
+++ python_stack/django_projects/tl_08/tl_08/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'^', include('apps.user_login.urls')),
     url(r'^dojo_ninjas', include('apps.dojo_ninjas.urls')),
+    url(r'^books_authors', include('apps.books_authors.urls')),    
 ]
