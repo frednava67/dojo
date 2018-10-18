349e74c2cac57878d374c9cd0c5d8c7513d06d34
Fred Nava <frednava67@hotmail.com>
2018-10-18 00:24:38 -0700

    final evening push for the night

--- python_stack/django_projects/belt_reviewer/apps/bookreviews/urls.py
+++ python_stack/django_projects/belt_reviewer/apps/bookreviews/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    # url(r'^process_registration$', views.process_registration),
-    # url(r'^process_login$', views.process_login),    
-    url(r'^books$', views.show_home),        
+    url(r'^books$', views.show_home),  
+    url(r'^books/add$ß', views.add),     
+    url(r'^logoff$', views.logoff),        
     url(r'^reset$', views.reset),            
 ]
--- python_stack/django_projects/belt_reviewer/apps/bookreviews/views.py
+++ python_stack/django_projects/belt_reviewer/apps/bookreviews/views.py
@@ -17,115 +17,26 @@ def index(request):
     if "user_id" not in request.session:
         return redirect("/login_registration")
         
-    recentreviews = get_three_most_recent_reviews()
-    print(recentreviews)
-
-    context = {   
-        # "first_name": request.session["first_name"],
-        # "last_name": request.session["last_name"],
-        # "email": request.session["email"],
-        "threerecentreviews": recentreviews
-    }
-
-    return render(request, "bookshome.html", context)
-
-# def process_registration(request):
-#     print("process_registration()")
-
-#     bFlashMessage = False
-
-#     if request.method == "POST":
-#         # First Name - Required; No fewer than 2 characters; letters only
-#         f_name = request.POST['first_name']
-#         if len(f_name) < 2 or NAME_REGEX.search(f_name):
-#             print("f_name failed validation")
-#             messages.error(request, u"First Name can only contain letters and be at least 2 characters", extra_tags="fname")
-#             bFlashMessage = True        
-
-#         # Last Name - Required; No fewer than 2 characters; letters only
-#         l_name = request.POST['last_name']
-#         if len(l_name) < 2 or NAME_REGEX.search(l_name):
-#             print("l_name failed validation")
-#             messages.error(request, u"Last Name can only contain letters and be at least 2 characters", extra_tags="lname")
-#             bFlashMessage = True     
-
-#         # Email - Required; Valid Format
-#         email = request.POST['email']
-#         if not EMAIL_REGEX.match(email):
-#             messages.error(request, u"Invalid Email Address!", extra_tags="email")
-#             bFlashMessage = True      
-
-#         #Email shouldn't be in database already
-#         i = User.objects.filter(email=email).count()
-#         if (i != 0):
-#             messages.error(request, u"Email Address already registered, please login.", 'email')
-#             bFlashMessage = True  
-
-#         # Password - Required; No fewer than 8 characters in length; matches Password Confirmation
-#         pwd = request.POST['password']
-#         confpwd = request.POST['confirmpw']
-
-#         # Password should be more than 8 characters
-#         if len(pwd) < 8:
-#             messages.error(request,u"Password must be at least 8 characters in length.", extra_tags='pwd')
-#             bFlashMessage = True
-
-#         # Password and Password Confirmation should match
-#         if pwd != confpwd:
-#             messages.error(request,"Password fields do not match!", extra_tags='pwd')
-#             bFlashMessage = True
-
-#         if bFlashMessage:
-#             request.session.clear()
-#             request.session["first_name"] = f_name
-#             request.session["last_name"] = l_name
-#             request.session["email"] = email                        
-#             return redirect(index)
-#         else:
-#             request.session.clear()
-#             request.session["first_name"] = f_name
-#             passwordToHash = pwd
-#             pwhash = bcrypt.hashpw(passwordToHash.encode(), bcrypt.gensalt())
-#             #new_id = User.objects.create(first_name=f_name, last_name=l_name, email=email, pwhashval=pwhash.decode())
-#             User.objects.create(first_name=f_name, last_name=l_name, email=email, pwhashval=pwhash.decode())
-#             #request.session["user_id"] = new_id
-
-#     return redirect(show_home)    
-
-# def process_login(request):
-#     print("process_login")
-#     print(request.POST['loginemail'])
-#     print(request.POST['loginpassword'])        
-#     if request.method == "POST":
-#         print("INSIDE IF")
-#         print(request.POST['loginemail'])
-#         loginemail = request.POST['loginemail']
-#         loginpassword = request.POST['loginpassword']
-#         obj = User.objects.filter(email=loginemail)
-#         i = obj.count()
-#         if (i > 0):
-#             tempHash = obj[0].pwhashval
-#             print(tempHash)
-#             bPasswordCheck = bcrypt.checkpw(loginpassword.encode(), tempHash.encode())
-#             print("bPasswordCheck", bPasswordCheck)
-#             request.session.clear()
-#             if (i == 0 or bPasswordCheck != True):
-#                 request.session["loginemail"] = loginemail                        
-#                 messages.error(request, u"You were not able to login.", 'login')
-#                 return redirect(index)
-#             else:
-#                 request.session["first_name"] = obj[0].first_name
-#                 request.session["user_id"] = obj[0].id
-#         else:
-#             request.session["loginemail"] = loginemail                        
-#             messages.error(request, u"You were not able to login.", 'login')
-#             return redirect(index)
-
-#     return redirect("/books")       
+    return redirect("/books")
 
 def show_home(request):
     print("show_home()")
-    return render(request, "bookshome.html")
+
+    if "user_id" not in request.session:
+        return redirect("/login_registration")
+
+    all_reviews_newest_first = Review.objects.order_by("-created_at")
+    recent_reviews = get_three_most_recent_reviews()
+    other_reviews = get_reviews_after_the_three_most_recent()
+
+    context = {   
+        "tempratinglist": {1,2,3,4,5},
+        "allreviews": all_reviews_newest_first,
+        "threerecentreviews": recent_reviews,
+        "otherreviews": other_reviews,
+    }
+
+    return render(request, "bookshome.html", context)
 
 def get_three_most_recent_reviews():
     print("get_three_most_recent_reviews()")
@@ -140,6 +51,33 @@ def get_three_most_recent_reviews():
             break
     
     return three_recent_reviews
+
+def get_reviews_after_the_three_most_recent():
+    print("get_reviews_after_the_three_most_recent()")
+    after_three_recent_reviews = []
+    all_reviews_newest_first = Review.objects.order_by("-created_at")
+
+    counter = 1
+    for review in all_reviews_newest_first:
+        if (counter > 3):
+            print(counter)
+            after_three_recent_reviews.append(review)
+        counter+=1
+    
+    return after_three_recent_reviews
+
+def add(request):
+    return render(request, "addbook.html")
+
+def process_add(request):
+
+
+    return HttpResponse("PROCESS ADD")
+
+def logoff(request):
+    request.session.clear()
+    return redirect('/')
+
 
 def reset(request):
     request.session.clear()
